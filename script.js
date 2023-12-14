7d4c36ae6b8846d74cac7f0759ce0a028a79ab2e
David Galvez Rodriguez <d_rodriguez@gmx.de>
2023-12-14 12:01:37 +0100

    second script

--- script.js
+++ script.js
@@ -30,8 +30,27 @@ let questions = [
         "answer_3": "Xenon (Xe)",
         "answer_4": "Wasserstoff (H)",
         "right_answer": 3
-    },
+    }
 
 
 
 ];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('allQuestions').innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+
+
